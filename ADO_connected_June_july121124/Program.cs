bbe5b448d17bfb677c199fe62f309a5b9fc06945
Acer <Acer@DESKTOP-ABKHEEV>
2024-11-14 09:51:59 +0530

    ADO.net Insert operation Added

--- ADO_connected_June_july121124/Program.cs
+++ ADO_connected_June_july121124/Program.cs
@@ -1,29 +1,76 @@
-﻿using System.Data.SqlClient;
-using System.Data;
-using System.Xml;
+﻿using System.Data;
+using System.Data.SqlClient;
 
 namespace ADO_connected_June_july121124
 {
     class CRUD_operation
     {
+        public void InsertOperation()
+        {
+            int id;
+            string name;
+            string div;
+            int totalMarks;
+            Console.WriteLine("Enter id,name ,Total Marks and div of Student");
+            id = int.Parse(Console.ReadLine());
+            name = Console.ReadLine();
+            totalMarks = int.Parse(Console.ReadLine());
+            div = Console.ReadLine();
+
+            string insertQry = $"insert into tblStudent(id,name,totalMarks,div)values({id},'{name}',{totalMarks},'{div}')";
+
+
+            //ADO.NET logic
+
+            SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=StudDB;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand(insertQry, con);
+            cmd.CommandType = CommandType.Text;
+            int n=0;
+            try
+            {
+                n = cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Id must be unique!!!!!");
+            }
+
+            if (n > 0)
+            {
+                Console.WriteLine("Record inserted Successfully...");
+            }
+            else
+            {
+                Console.WriteLine("record not inserted!!!!");
+            }
+            con.Close();
+
+
+
+
+
+
+
+        }
         public void selectOperation()
         {
             SqlConnection con = new SqlConnection();
-            con.ConnectionString= @"Data Source=DESKTOP-ABKHEEV;Initial Catalog=EmpDB;Integrated Security=True;TrustServerCertificate=True";
+            con.ConnectionString = @"Data Source=DESKTOP-ABKHEEV;Initial Catalog=EmpDB;Integrated Security=True;TrustServerCertificate=True";
             con.Open();
 
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = con;
             cmd.CommandText = "select * from tblEmployee";
-           // cmd.CommandText = "ProSelect";
+            // cmd.CommandText = "ProSelect";
             cmd.CommandType = CommandType.Text;
-            SqlDataReader rdr= cmd.ExecuteReader();
+            SqlDataReader rdr = cmd.ExecuteReader();
             while (rdr.Read())
             {
-                Console.WriteLine(rdr[0].ToString()+" " + rdr[1].ToString()+" " + rdr[2].ToString());
+                Console.WriteLine(rdr[0].ToString() + " " + rdr[1].ToString() + " " + rdr[2].ToString());
             }
             con.Close();
-        }   
+        }
     }
     internal class Program
     {
@@ -64,7 +111,8 @@ namespace ADO_connected_June_july121124
             // con.Close();
 
             CRUD_operation cr1 = new CRUD_operation();
-            cr1.selectOperation();
+            // cr1.selectOperation();
+            cr1.InsertOperation();
         }
     }
 }
